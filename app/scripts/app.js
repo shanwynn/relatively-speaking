2fd4cd22a90406e13e40e5408e82ed01c070cd66
Shannon Wynn <shannonwynn@mac.com>
2014-12-04 11:42:43 -0500

    fixed error on recipe form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,13 +2,6 @@ var Rs = window.Rs = Ember.Application.create({
 	LOG_TRANSITIONS: true
 });
 
-Rs.CATEGORIES = [
-	{ id: '1', name: 'Main Dish'},
-	{ id: '2', name: 'Side Dish'},
-	{ id: '3', name: 'Bread'},
-	{ id: '4', name: 'Dessert'}
-];
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
@@ -18,3 +11,10 @@ require('scripts/components/*');
 require('scripts/views/*');
 require('scripts/router');
 require('scripts/authentication');
+
+Rs.CATEGORIES = [
+	{ id: '1', name: 'Main Dish'},
+	{ id: '2', name: 'Side Dish'},
+	{ id: '3', name: 'Bread'},
+	{ id: '4', name: 'Dessert'}
+];
