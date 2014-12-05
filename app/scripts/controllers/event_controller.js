4c35e2e621b1b9b1757e441af609c9a682db8937
Shannon Wynn <shannonwynn@mac.com>
2014-12-05 09:11:38 -0500

    added event controller, model and template

---
+++ app/scripts/controllers/event_controller.js
@@ -0,0 +1,13 @@
+Rs.EventController = Ember.Controller.extend({
+  actions: {
+    save: function() {
+      var event = this.store.createRecord('event', {
+        title:    this.get('title'),
+        date:     this.get('date'),
+        post:     this.get('post'),
+      });
+        event.save();
+      this.transitionToRoute('events');
+    }
+  }
+});
---
+++ app/scripts/models/event_model.js
@@ -0,0 +1,5 @@
+Rs.Event = DS.Model.extend({
+  title: DS.attr('string'),
+  date: DS.attr('string'),
+  post: DS.attr('string')
+});
