fe5f0bbbf7990c5688eda555979765127d78b58b
Shannon Wynn <shannonwynn@mac.com>
2014-12-07 21:43:07 -0500

    I've got CRUD

--- app/scripts/controllers/edit_news_controller.js
+++ app/scripts/controllers/edit_news_controller.js
@@ -3,6 +3,11 @@ actions: {
   update: function () {
     this.model.save();
     this.transitionToRoute('news');
+   },
+  destroy: function () {
+    this.get('model').deleteRecord();
+    this.get('model').save();
+    this.transitionToRoute('news');
+   }
   }
-}
 });
